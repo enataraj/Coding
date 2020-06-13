2c0bd7f211d3ca1aa0c2c64e0147d7edc95de84e
Elumalai <vnselumalai@gmail.com>
2020-06-13 16:58:37 -0700

    Hello List

---
+++ src/list/HelloList.java
@@ -0,0 +1,18 @@
+package list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HelloList {
+	
+	public static void main(String[] args) {
+		List<String> list = new ArrayList<>();
+		list.add("Elumalai");
+		list.add("Kanimozhi");
+		list.add("Nishanth");
+		list.add("Krish");
+		
+		System.out.println(list);
+	} 
+
+}

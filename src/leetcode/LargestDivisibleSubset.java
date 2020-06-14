786dee3444741d5a354ac18cac95a53a7435c6ea
enataraj <enataraj@cisco.com>
2020-06-13 22:04:17 -0700

    Solution for LargestDivisibleSubset

--- src/leetcode/LargestDivisibleSubset.java
+++ src/leetcode/LargestDivisibleSubset.java
@@ -2,36 +2,51 @@ package leetcode;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashSet;
+import java.util.Collections;
 import java.util.List;
-import java.util.Set;
 
 public class LargestDivisibleSubset {
     public List<Integer> largestDivisibleSubset(int[] nums) {
-        if (nums.length == 1) {
-            return Arrays.asList(nums[0]);
+        int n = nums.length;
+        if (n == 0) {
+            return Collections.emptyList();
         }
-        Set<Integer> result = new HashSet<>();
-        for (int i = 0; i < nums.length; i++) {
-            int count = 0;
-            for (int j = 0; j < nums.length; j++) {
-                if (nums[i] % nums[j] == 0 || nums[j] % nums[i] == 0) {
-                    count++;
 
-                } else {
-                    break;
-                }
-                if (count == nums.length - 1) {
-                    result.add(nums[i]);
+        Arrays.sort(nums);
+        int[] dp = new int[n];
+
+        Arrays.fill(dp, 1);
+        int maxSubsetSize = 1, maxIdx = 0;
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] % nums[j] == 0) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                    if (dp[i] > maxSubsetSize) {
+                        maxSubsetSize = dp[i];
+                        maxIdx = i;
+                    }
                 }
             }
         }
-        return new ArrayList<Integer>(result);
+        System.out.println(Arrays.toString(dp));
+        List<Integer> res = new ArrayList<>();
+        res.add(nums[maxIdx]);
+        int subsetSize = maxSubsetSize - 1; // next subset size to look for in DP array
 
+        for (int i = maxIdx - 1; i >= 0; i--) {
+            if (nums[maxIdx] % nums[i] == 0 && dp[i] == subsetSize) {
+                res.add(nums[i]);
+                --subsetSize;
+            }
+        }
+
+        Collections.reverse(res);
+        return res;
     }
 
     public static void main(String[] args) {
-        int nums[] = { 3, 4, 16, 8 };
+        int nums[] = { 1, 2, 3, 6, 9, 10 };
         List<Integer> res = new LargestDivisibleSubset().largestDivisibleSubset(nums);
         System.out.println(res);
     }
